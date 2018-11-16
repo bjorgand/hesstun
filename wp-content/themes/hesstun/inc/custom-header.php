ddd26b5be45b8c4e5100a85612178ff2ca53c63f
Bjørg Helene Andorsen <bjorg.andorsen@gmail.com>
2018-11-16 16:02:18 +0100

    «Colours and @media-queries»
    Started on the header.php and the custom-header-php. Changed the background image and styles for the header.

--- wp-content/themes/hesstun/inc/custom-header.php
+++ wp-content/themes/hesstun/inc/custom-header.php
@@ -19,7 +19,7 @@
 function hesstun_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'hesstun_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
+		'default-text-color'     => 'ffffff',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
